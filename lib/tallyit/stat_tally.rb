1114f19451fde0d7ceefc62b2e677c26a87d8b26
hzlu <hzlu2010@163.com>
2016-03-26 22:08:13 +0800

    add color to output

--- lib/tallyit/stat_tally.rb
+++ lib/tallyit/stat_tally.rb
@@ -12,18 +12,24 @@ module Tallyit
           File.open(file, 'r') do |file|
             file.each_with_index do |line, index|
               tmp = line.chomp.split(' | ')
-              tmp[0] == 'income' ? income_amount << tmp[1].to_f : expend_amount << tmp[1].to_f
+              if tmp[0] == 'income'
+                income_amount << tmp[1].to_f
+                tmp[1] = Rainbow(tmp[1]).green
+              else
+                expend_amount << tmp[1].to_f
+                tmp[1] = Rainbow(tmp[1]).red
+              end
               t << tmp.unshift(index + 1)
             end
           end
           income_total = income_amount.reduce(0, :+)
           expend_total = expend_amount.reduce(0, :+)
           t << :separator
-          t << ['Income total', income_total]
+          t << ['Income total', Rainbow(income_total).green]
           t << :separator
-          t << ['Expend total', expend_total]
+          t << ['Expend total', Rainbow(expend_total).red]
           t << :separator
-          t << ['Remaining', income_total - expend_total]
+          t << ['Remaining', Rainbow(income_total - expend_total).blue.inverse]
         end
         puts table
       elsif format == :raw
--- tallyit.gemspec
+++ tallyit.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency "terminal-table", "~> 1.5"
+  spec.add_runtime_dependency "rainbow", "~> 2.1"
   spec.add_development_dependency "bundler", "~> 1.9"
   spec.add_development_dependency "rake", "~> 10.0"
 end
